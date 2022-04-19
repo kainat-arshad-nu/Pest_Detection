cb4c70ea654c33165c8fdb6347b16c7fe90617af
Syed-Adnan-Tariq <98732460+kainat-arshad-nu@users.noreply.github.com>
2022-04-19 20:50:03 +0500

    Added requirements.txt dependencies docker container

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, flash, request, redirect, url_for, render_template
-import urllib.request
 import os
 from werkzeug.utils import secure_filename
-import pickle
 from tensorflow import keras
 
 app = Flask(__name__)
