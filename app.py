f805fb9db7bdb22f573911c2e3c4cd971c6d339e
Syed-Adnan-Tariq <98732460+kainat-arshad-nu@users.noreply.github.com>
2022-04-22 12:34:00 +0500

    tweaks for deployment

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# UPLOAD_FOLDER = "static/uploads/"
-UPLOAD_FOLDER = url_for("static/uploads/")
+UPLOAD_FOLDER = "static/uploads/"
+# UPLOAD_FOLDER = url_for("static/uploads/")
 
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
